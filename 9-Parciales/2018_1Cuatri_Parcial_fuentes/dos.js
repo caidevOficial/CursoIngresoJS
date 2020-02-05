e017dc21ec6a33cddbae0f3aef4a657d05f37105
caidevOficial <falcone.facu@gmail.com>
2020-02-05 01:38:11 -0300

    Add files via upload
    Primer commit de Ejs Parciales 2018 (1 a 3)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,11 @@
+/* mostrar por alert el mensaje "usted es  xxxxx y vive en la localidad de xxxxxxxx".*/
 function mostrar()
 {
-  
+  var name;
+  var location;
+
+  name = document.getElementById('elNombre').value;
+  location = document.getElementById('laLocalidad').value;
+
+  Alert("Usted es " + name + " y vive en " + location);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
+/* Realizar el algoritmo que  pida el ancho y el largo 
+de un rectángulo por prompt y que muestre el perímetro por alert.*/
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var calcularPerimetro;
 
+	ancho = parseInt(prompt("Por favor, ingrese el ancho del rectangulo: "));
+	largo = parseInt(prompt("Por favor, ingrese el largo del rectangulo: "));
+
+	calcularPerimetro = (ancho + largo) * 2;
+
+	alert("El perímetro del rectángulo es: " + calcularPerimetro)
 }
