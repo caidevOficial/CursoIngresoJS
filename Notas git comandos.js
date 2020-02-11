a0568fd602f60c47da338f7940f6065f8b73f4d0
caidevOficial <caidev.sama@gmail.com>
2020-02-11 20:10:34 -0300

    prueba de subida desde consola 4

---
+++ Notas git comandos.js
@@ -0,0 +1,8 @@
+/*
+Comandos: 
+git clone -> git status
+git add .  -> git status
+git commit -m "Comentario"
+git push (o sino )
+git push https://github.com/caidevOficial/CursoIngresoJS master
+*/
