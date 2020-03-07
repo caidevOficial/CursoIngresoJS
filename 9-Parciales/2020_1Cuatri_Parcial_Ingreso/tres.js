5a4c5e0be7ee7d838199fd113722c4621850e063
caidevoficial <caidev.sama@gmail.com>
2020-03-07 01:38:54 -0300

    parcial 2020 decoracion

---
+++ 9-Parciales/2020_1Cuatri_Parcial_Ingreso/tres.js
@@ -0,0 +1,24 @@
+/*
+	En el ingreso a un viaje en crucero nos solicitan:<br>
+        1 - NOMBRE<br>
+        2 - EDAD (mayores a 18 años)<br>
+        3 - Sexo ("m" o "f")<br>
+        4 - Estado Civil ("soltero", "casado", "viudo").<br><br>
+        A) Dar el nombre del hombre casado mas joven.<br>
+        B) El sexo y nombre del pasajero/a mas viejo.<br>
+        C) La cantidad de mujeres que hay casadas o viudas.<br>
+        D) El promedio de edad entre las mujeres.<br>
+        E) El promedio de edad entre los hombres solteros.
+*/
+function mostrar()
+{
+	
+
+//························································
+// RESPUESTAS
+
+document.write(" "+  + "<br>");
+document.write(" "+  + "<br>");
+document.write(" "+  + "<br>");
+document.write(" "+  + "<br>");
+}
